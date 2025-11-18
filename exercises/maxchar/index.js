8a15f3f8c3dea63a5f0feb3462d9b1896a621aa1
Shaurya <shaurya26@gmail.com>
2025-11-18 13:04:36 +0530

    Update index.js

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -6,9 +6,27 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-    // let charMap = {}
+
     let charMap = new Map();
+    let max = 0;
+    let char;
+    let maxChar = '';
 
+    for (char of str) {
+        if (charMap.has(char)) {
+            charMap.set(char, charMap.get(char) + 1)
+        } else {
+            charMap.set(char, 1)
+        }
+    }
+    let myMapIter = charMap.keys()
+
+    for (key of myMapIter) {
+        if (charMap.get(key) > max) {
+            maxChar = key
+            max = charMap.get(key)
+        }
+    }
+    return maxChar;
 }
-
 module.exports = maxChar;
